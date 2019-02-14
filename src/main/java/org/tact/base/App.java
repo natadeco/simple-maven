2a80f9a429f88d010a3fc1c9dee032b5d4f9d3c2
natadeco <alikings97.sa@gmail.com>
2019-02-14 19:54:12 +0530

    printing statement updated

--- src/main/java/org/tact/base/App.java
+++ src/main/java/org/tact/base/App.java
@@ -3,6 +3,6 @@ package org.tact.base;
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println("Maven is awesome");
+		System.out.println("Subashini mam is awesome");
 	}
 }
