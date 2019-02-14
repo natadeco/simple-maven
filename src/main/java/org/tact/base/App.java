a6843548a004e50821a9e6733cba45e9d7aab2d0
natadeco <alikings97.sa@gmail.com>
2019-02-14 20:13:14 +0530

    Subtraction Added

--- src/main/java/org/tact/base/App.java
+++ src/main/java/org/tact/base/App.java
@@ -5,8 +5,11 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("Subashini mam is awesome");
 		
-		int  c = MathUtil.add(8, 9);
+		int c = MathUtil.add(8, 9);
 		System.out.println(c);
+		
+		int d = MathUtil.subtract(8, 9);
+		System.out.println(d);
 	}
 	
 	
