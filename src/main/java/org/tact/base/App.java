987b5471480f68637e4083cbedb26754f10e923d
natadeco <alikings97.sa@gmail.com>
2019-02-14 20:01:41 +0530

    Addition module is added

--- src/main/java/org/tact/base/App.java
+++ src/main/java/org/tact/base/App.java
@@ -4,5 +4,12 @@ public class App {
 
 	public static void main(String[] args) {
 		System.out.println("Subashini mam is awesome");
+		
+		int c = add(8, 9);
+		System.out.println(c);
+	}
+	
+	public static int add(int a, int b) {	
+		return a+b;		
 	}
 }
